65111ca4e971d1a9700bcee358e497fd1d3eaa51
Alan Maldonado <alan.m@incfile.com>
2024-03-15 12:04:52 -0600

    ✨ Mutations

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,19 @@
 import TheHeader from "@/components/TheHeader.vue";
 import ProductCard from "@/components/ProductCard.vue";
 import { useProductStore } from '@/stores/ProductStore';
+import { useCartStore } from '@/stores/CartStore';
 import { storeToRefs } from "pinia";
 
 const productStore = useProductStore();
+const cartStore = useCartStore();
 productStore.fill();
 const { products } = storeToRefs(productStore);
+const addToCart = (count, product) => {
+  count = parseInt(count);
+  for (let index = 0; index < count; index++) {
+    cartStore.items.push(product)
+  }
+}
 
 </script>
 
@@ -14,7 +22,8 @@ const { products } = storeToRefs(productStore);
   <div class="container">
     <TheHeader />
     <ul class="sm:flex flex-wrap lg:flex-nowrap gap-5">
-      <ProductCard v-for="product in products" :key="product.name" :product="product" />
+      <ProductCard v-for="product in products" :key="product.name" :product="product"
+        @addToCart="addToCart($event, product)" />
     </ul>
   </div>
 </template>
