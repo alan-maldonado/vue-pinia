27facb2c151e6fcfa341f2130a1090cfc3d6c908
Alan Maldonado <alan.m@incfile.com>
2024-03-15 14:35:46 -0600

    🔧 Options API with pinia

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,14 +1,23 @@
-<script setup>
+<script>
 // imports
+import { useAuthUserStore } from '../stores/AuthUserStore';
+import { mapState } from 'pinia';
 import CartWidget from './CartWidget.vue';
+export default {
+  components: { CartWidget },
+  computed: {
+    ...mapState(useAuthUserStore, ['username'])
+  }
+}
 </script>
 
 <template>
-  <header 
-    class="flex justify-between p-6 mb-10 items-center"
-    style="background-image: url('/images/double-bubble-outline.png')"
-  >
+  <header class="flex justify-between p-6 mb-10 items-center"
+    style="background-image: url('/images/double-bubble-outline.png')">
     <h1 class="text-4xl text-gray-700 font-bold">The Pineapple Stand</h1>
-    <CartWidget/>
+    <div class="mr-5">
+      <span>{{ username }}</span>
+      <CartWidget class="inline-block" />
+    </div>
   </header>
 </template>
