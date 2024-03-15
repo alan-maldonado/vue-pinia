b4f0ec2a206a21eee1a0ec7955fc754ea0790ba8
Alan Maldonado <alan.m@incfile.com>
2024-03-15 11:49:30 -0600

    ♻️ StoreToRefs

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,9 @@
 import TheHeader from "@/components/TheHeader.vue";
 import ProductCard from "@/components/ProductCard.vue";
 import { useProductStore } from '@/stores/ProductStore';
+import { storeToRefs } from "pinia";
 
-const productStore = useProductStore();
+const { products } = storeToRefs(useProductStore());
 
 </script>
 
@@ -11,7 +12,7 @@ const productStore = useProductStore();
   <div class="container">
     <TheHeader />
     <ul class="sm:flex flex-wrap lg:flex-nowrap gap-5">
-      <ProductCard v-for="product in productStore.products" :key="product.name" :product="product" />
+      <ProductCard v-for="product in products" :key="product.name" :product="product" />
     </ul>
   </div>
 </template>
