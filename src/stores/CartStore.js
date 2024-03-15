1d435bb2b6dd0a7896fcd3cb1cb524adae752a27
Alan Maldonado <alan.m@incfile.com>
2024-03-15 14:51:08 -0600

    🚀 HMRUpdate

--- src/stores/CartStore.js
+++ src/stores/CartStore.js
@@ -1,4 +1,4 @@
-import { defineStore } from 'pinia';
+import { defineStore, acceptHMRUpdate } from 'pinia';
 import _ from 'lodash';
 import { useAuthUserStore } from './AuthUserStore';
 export const useCartStore = defineStore('CartStore', {
@@ -46,3 +46,7 @@ export const useCartStore = defineStore('CartStore', {
     },
   },
 });
+
+if (import.meta.hot) {
+  import.meta.hot.accept(acceptHMRUpdate(useCartStore, import.meta.hot));
+}
