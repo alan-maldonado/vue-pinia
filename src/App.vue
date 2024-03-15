1b531415e4caa6b574195e41bb7b4886d693dacb
Alan Maldonado <alan.m@incfile.com>
2024-03-15 15:33:03 -0600

    ♻️ Make undo redo part of a pinia plugin

--- src/App.vue
+++ src/App.vue
@@ -3,41 +3,12 @@ import TheHeader from "@/components/TheHeader.vue";
 import ProductCard from "@/components/ProductCard.vue";
 import { useProductStore } from '@/stores/ProductStore';
 import { useCartStore } from '@/stores/CartStore';
-import { ref, reactive } from "vue";
 
 const productStore = useProductStore();
 const cartStore = useCartStore();
-const history = reactive([]);
-const future = reactive([]);
-const doingHistory = ref(false);
-history.push(JSON.stringify(cartStore.$state));
 
-const redo = () => {
-  const lastState = future.pop();
-  if (!lastState) return;
-  doingHistory.value = true;
-  history.push(lastState);
-  cartStore.$state = JSON.parse(lastState);
-  doingHistory.value = false;
-}
-
-const undo = () => {
-  if (history.length === 1) return;
-  doingHistory.value = true;
-  future.push(history.pop());
-  cartStore.$state = JSON.parse(history.at(-1));
-  doingHistory.value = false;
-}
-
-cartStore.$subscribe((mutation, state) => {
-  if (!doingHistory.value) {
-    history.push(JSON.stringify(state));
-    future.splice(0, future.length);
-  }
-});
 
 cartStore.$onAction(({
-
   name,
   store,
   args,
@@ -62,8 +33,8 @@ productStore.fill();
   <div class="container">
     <TheHeader />
     <div class="mb-5 flex justify-end">
-      <AppButton @click="undo">Undo</AppButton>
-      <AppButton class="ml-2" @click="redo">Redo</AppButton>
+      <AppButton @click="cartStore.undo">Undo</AppButton>
+      <AppButton class="ml-2" @click="cartStore.redo">Redo</AppButton>
     </div>
     <ul class="sm:flex flex-wrap lg:flex-nowrap gap-5">
       <ProductCard v-for="product in productStore.products" :key="product.name" :product="product"
---
+++ src/plugins/PiniaHistoryPlugin.js
@@ -0,0 +1,32 @@
+import { ref, reactive } from 'vue';
+export function piniaHistoryPlugin({ pinia, app, store, options }) {
+  const history = reactive([]);
+  const future = reactive([]);
+  const doingHistory = ref(false);
+  history.push(JSON.stringify(store.$state));
+
+  store.$subscribe((mutation, state) => {
+    if (!doingHistory.value) {
+      history.push(JSON.stringify(state));
+      future.splice(0, future.length);
+    }
+  });
+
+  return {
+    undo: () => {
+      if (history.length === 1) return;
+      doingHistory.value = true;
+      future.push(history.pop());
+      store.$state = JSON.parse(history.at(-1));
+      doingHistory.value = false;
+    },
+    redo: () => {
+      const lastState = future.pop();
+      if (!lastState) return;
+      doingHistory.value = true;
+      history.push(lastState);
+      store.$state = JSON.parse(lastState);
+      doingHistory.value = false;
+    },
+  };
+}
