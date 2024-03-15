957422f7adb325d105721be0d3c39353538c7741
Alan Maldonado <alan.m@incfile.com>
2024-03-15 12:25:25 -0600

    ♻️ Mutate state with actions

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,10 @@ import TheHeader from "@/components/TheHeader.vue";
 import ProductCard from "@/components/ProductCard.vue";
 import { useProductStore } from '@/stores/ProductStore';
 import { useCartStore } from '@/stores/CartStore';
-import { storeToRefs } from "pinia";
 
 const productStore = useProductStore();
 const cartStore = useCartStore();
 productStore.fill();
-const { products } = storeToRefs(productStore);
-const addToCart = (count, product) => {
-  count = parseInt(count);
-
-  cartStore.$patch(state => {
-    for (let index = 0; index < count; index++) {
-      state.items.push(product)
-    }
-  });
-}
 
 </script>
 
@@ -25,8 +14,8 @@ const addToCart = (count, product) => {
   <div class="container">
     <TheHeader />
     <ul class="sm:flex flex-wrap lg:flex-nowrap gap-5">
-      <ProductCard v-for="product in products" :key="product.name" :product="product"
-        @addToCart="addToCart($event, product)" />
+      <ProductCard v-for="product in productStore.products" :key="product.name" :product="product"
+        @addToCart="cartStore.addItems($event, product)" />
     </ul>
   </div>
 </template>
